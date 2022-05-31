b4c3e391a5203fd759fa01a30e6b452f3fb43e77
root <root@virtualserver01potopenshiftappmodern.IBM-PoC-CSM-Lab.cloud>
2022-05-31 09:21:38 -0500

    rollback changes introduced 21 days ago

--- terraform/stages-ocp4/stage2-cicd.tf
+++ terraform/stages-ocp4/stage2-cicd.tf
@@ -1,9 +1,10 @@
 module "cicd" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v1.11.0"
+  source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v1.7.7"
 
   cluster_config_file = module.dev_cluster.config_file_path
   cluster_type        = module.dev_cluster.type_code
   olm_namespace       = module.dev_software_olm.olm_namespace
   operator_namespace  = module.dev_software_olm.target_namespace
   ingress_subdomain   = module.dev_cluster.ingress_hostname
+  app_namespace       = module.dev_tools_namespace.name
 }
