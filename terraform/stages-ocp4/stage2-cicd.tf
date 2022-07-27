3919c3fbfa7b326a9c0af12c250211c272ca7ff1
root <root@virtualserver01.IBM-PoC-CSM-Lab.cloud>
2022-07-27 15:30:28 -0500

    last changes

--- terraform/stages-ocp4/stage2-cicd.tf
+++ terraform/stages-ocp4/stage2-cicd.tf
@@ -1,6 +1,6 @@
 module "cicd" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v1.7.7"
-
+  #source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v1.7.7"
+  source = "github.com/cloud-native-toolkit/terraform-tools-openshift-cicd.git?ref=v1.11.0"
   cluster_config_file = module.dev_cluster.config_file_path
   cluster_type        = module.dev_cluster.type_code
   olm_namespace       = module.dev_software_olm.olm_namespace
